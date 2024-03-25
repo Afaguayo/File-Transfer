845a40bc36e9b05013eaa695afbbce8b3b2f9285
afaguayo <afaguayo@miners.utep.edu>
2024-03-25 11:19:57 -0600

    wrk in pgrs

--- single_threaded_src/server.py
+++ single_threaded_src/server.py
@@ -1,17 +1,17 @@
 #! /usr/bin/env python3
-
-# Echo server program
-
 import socket, sys, re
 sys.path.append("../lib")       # for params
 import params
+import io
+
+sys.path.append("../Archiver")  # for Archiver
+from extract import outBandExtract
+
 
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
     (('-?', '--usage'), "usage", False), # boolean (set if present)
     )
-
-
 
 progname = "echoserver"
 paramMap = params.parseParams(switchesVarDefaults)
@@ -25,20 +25,33 @@ if paramMap['usage']:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((listenAddr, listenPort))
 s.listen(1)              # allow only one outstanding request
-# s is a factory for connected sockets
 
 conn, addr = s.accept()  # wait until incoming connection request (and accept it)
 print('Connected by', addr)
-while 1:
-    data = conn.recv(1024).decode()
+
+
+while True:
+    data = conn.recv(1024)
+
     if len(data) == 0:
         print("Zero length read, nothing to send, terminating")
         break
-    sendMsg = ("Echoing %s" % data).encode()
-    print("Received '%s', sending '%s'" % (data, sendMsg.decode()))
-    while len(sendMsg):
-        bytesSent = conn.send(sendMsg)
-        sendMsg = sendMsg[bytesSent:0]
+    
+    extractedFilesMsg = outBandExtract(data)
+
+    for fileInfo in extractedFilesMsg:
+        sendMsg = b"\nEchoing "
+        sendMsg += b"\nFile name: " + fileInfo['name'].encode() 
+        sendMsg += b"\nFile size: " + str(fileInfo['size']).encode() 
+        sendMsg += b"\nFile contents: " + fileInfo['contents'].encode()
+
+        data = "\nFile name: " + fileInfo['name'] + "\nFile size: " + str(fileInfo['size']) +  "\nFile contents: " + fileInfo['contents']
+        print("Received \n'%s', \nsending \n'%s'" % (data, sendMsg))
+
+        while len(sendMsg):
+            bytesSent = conn.send(sendMsg)
+            sendMsg = sendMsg[bytesSent:0]
+
+        data = ""
 conn.shutdown(socket.SHUT_WR)
 conn.close()
-
